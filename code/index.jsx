df50d984b701bbfd34025025a90ca3d664aa0d03
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-04-23 10:13:15 -0400

    extracted messages

--- code/index.jsx
+++ code/index.jsx
@@ -1,8 +1,8 @@
 export default () => {
   return (
     <body>
-      <h1>Welcome to our website</h1>
-      <p>We are the company behind inlang.com and lix.</p>
+      <h1>{t("huppy_easter")}</h1>
+      <p>{t("jupyter_notebook")}</p>
     </body>
   );
 };
